e09af956a93a2574c10882e2f5689cd494196237
MojitoSensei <142766161+MojitoSensei@users.noreply.github.com>
2023-09-27 17:37:12 +1100

    Add files via upload

--- server-http.mjs
+++ server-http.mjs
@@ -4,21 +4,43 @@ import fs from "node:fs/promises";
 const host = "localhost";
 const port = 8000;
 
-async function requestListener(_request, response) {
+async function requestListener(request, response) {
+  response.setHeader("Content-Type", "text/html");
   try {
     const contents = await fs.readFile("index.html", "utf8");
-
-    response.setHeader("Content-Type", "text/html");
-    response.writeHead(200);
-    response.end(contents);
+    const urlSegments = request.url.split("/");
+    switch (urlSegments[1]) {
+      case "":
+      case "index.html":
+        response.writeHead(200);
+        return response.end(contents);
+      case "random":
+        const nb = parseInt(urlSegments[2]);
+        if (!isNaN(nb)) {
+          const randomNumbers = generateRandomNumbers(nb);
+          response.writeHead(200);
+          return response.end(`<html><p>${randomNumbers.join("<br>")}</p></html>`);
+        }
+      default:
+        response.writeHead(404);
+        return response.end(`<html><p>404: NOT FOUND</p></html>`);
+    }
   } catch (error) {
     console.error(error);
-
-    response.setHeader("Content-Type", "text/plain");
-    response.writeHead(500); // Définit le code d'erreur 500
-    response.end("Erreur interne du serveur (500) : Le fichier index.html est introuvable.");
+    response.writeHead(500);
+    return response.end(`<html><p>500: INTERNAL SERVER ERROR</p></html>`);
   }
 }
+
+function generateRandomNumbers(count) {
+  const randomNumbers = [];
+  for (let i = 0; i < count; i++) {
+    randomNumbers.push(Math.floor(100 * Math.random()));
+  }
+  return randomNumbers;
+}
+
+
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
